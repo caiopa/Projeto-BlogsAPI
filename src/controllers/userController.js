324a276060f5a5f7f87205ce2364b5bcda89a04d
Caio Pereira Antunes <caioefg@gmail.com>
2022-09-24 18:04:59 -0300

    arrumando

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,9 @@
 const userService = require('../service/userService');
 
 const insertUser = async (req, res, next) => {
-    console.log(req.body);
 try {
   return res.status(201).json(await userService.createUser(req.body));
 } catch (error) {
-    console.log(error.message);
   next(error);
   }
 };
