28681bb1ee9dd68de12934e4190f3589adf7de9e
Caio Pereira Antunes <caioefg@gmail.com>
2022-09-24 15:32:14 -0300

    req3

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY || 'suaSenhaSecreta';
 
